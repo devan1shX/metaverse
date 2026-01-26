f81935bc7f147a7680e709ece26fb02c97cc39f9
Anish <anish22075@iiitd.ac.in>
2026-01-26 11:01:32 +0530

    Update rw.sh

--- rw.sh
+++ rw.sh
@@ -1,4 +1,20 @@
 # run ws layer
 cd app/ws_layer
-source venv/bin/activate
+C:\Python312\python.exe -m venv venv
+.\venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 uvicorn main:app --host 0.0.0.0 --port 8003
+
+
+
+
+
+
+
+
+
+
+
+
+Delete : Remove-Item -Recurse -Force venv
